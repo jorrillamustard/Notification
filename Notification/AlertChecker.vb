85ca03efdce78b1dc7c58d1e86348905ad3eebbb
Brian Martin <bmartin5692@gmail.com>
2016-07-13 09:08:12 -0400

    Sev logo to function
    Sev logo to function

--- Notification/AlertChecker.vb
+++ Notification/AlertChecker.vb
@@ -15,35 +15,24 @@ Public Class AlertChecker
         My.Settings.Save()
         Dim query As New StringBuilder
         query.Append("(createDate >= DateTime.Parse(""")
-        query.Append(My.Settings.LastCheckTime.ToString("MM/dd/yyyy HH:mm"))
+        query.Append(My.Settings.LastCheckTime.ToString("MM/dd/yyyy HH:mm:ss"))
         query.Append("""))")
         Dim newa = RestClnt.Functions.Alert.GetAlertsWithCounts(,,,, query.ToString)
         If newa.Success = True Then
 
             For Each e In newa.Data.entities
 
-                Dim sevlogo As Image
-                    Select Case e.severity
-                        Case 1
-                            sevlogo = My.Resources.FtransparentRed
-                        Case 2
-                            sevlogo = My.Resources.FtransparentRed
-                        Case 3
-                            sevlogo = My.Resources.FtransparentOrange_1
-                        Case Else
-                            sevlogo = My.Resources.FtransparentYellow
-                    End Select
-                    Dim caption As String = "New Alert"
-                    Dim message As New StringBuilder
-                    message.AppendLine("Name: " & e.artifactName)
-                    message.AppendLine("Assessment: " & e.assessment)
-                    Dim sev As Functions.Sev = e.severity
-                    message.AppendLine("Severity: " & sev.ToString)
-                    message.AppendLine("Source: " & e.source)
-                    message.AppendLine("Target: " & e.target)
+                Dim sevlogo As Image = GetSevLogo(e.severity)
+                Dim caption As String = "New Alert"
+                Dim message As New StringBuilder
+                message.AppendLine("Name: " & e.artifactName)
+                message.AppendLine("Assessment: " & e.assessment)
+                Dim sev As Functions.Sev = e.severity
+                message.AppendLine("Severity: " & sev.ToString)
+                message.AppendLine("Source: " & e.source)
+                message.AppendLine("Target: " & e.target)
 
-                    Functions.NewAlert(caption, message.ToString, sevlogo, My.Settings.AlertTimeout)
-
+                Functions.NewAlert(caption, message.ToString, sevlogo, My.Settings.AlertTimeout)
 
             Next
 
@@ -57,6 +46,23 @@ Public Class AlertChecker
         AlertCheckTimer.Start()
     End Sub
 
+    Private Function GetSevLogo(ByVal severity As Integer)
+        Select Case severity
+            Case 1 'Critical
+                Return My.Resources.FtransparentRed
+            Case 2 'High
+                Return My.Resources.FtransparentRed
+            Case 3 'Medium
+                Return My.Resources.FtransparentOrange_1
+            Case 4 'Low
+                Return My.Resources.FtransparentYellow
+            Case 5 'Warning
+                Return My.Resources.FtransparentYellow
+                'Catch all
+            Case Else
+                Return My.Resources.flogo3232
+        End Select
+    End Function
     Private Sub AlertCheckTimer_Tick()
         CheckForAlerts()
     End Sub
