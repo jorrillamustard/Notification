0bebff9a92c45f26afb624125845e69035027ea1
Brian Martin <bmartin5692@gmail.com>
2016-07-14 17:07:11 -0400

    SQLite Database For Alert History
    SQLite Database For Alert History

--- Notification/Form_Hide.vb
+++ Notification/Form_Hide.vb
@@ -19,16 +19,21 @@ Public Class Form_Hide
         RestClient.Server = My.Settings.WebServer
         RestClient.IgnoreSSL = True
         RestClient.Authenticate()
+        If Not IO.File.Exists("NotificationDB.db3") Then
+            Debug.WriteLine("SQLDB NotificationDB.db3 not found...Creating...")
+            Functions.CreateNotificationDB()
+        Else
+            Debug.WriteLine("SQLDB NotificationDB.db3 found...Validating..")
+            Functions.ValidateNotificationDB()
+        End If
 
     End Sub
 
     Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
-
+        Me.Hide()
 
 
         If TestConfiguration(1) = True Then
-
-
             Functions.NewAlert("Endpoint Alert Notifications", "Endpoint Alert Notifications Started", My.Resources.flogo3232, 10000)
             ' Start Timer/Function
             If My.Settings.LastCheckTime < DateTime.Now.AddHours(-1) Then
@@ -41,7 +46,7 @@ Public Class Form_Hide
             My.Settings.Save()
             Form_Configure.Show()
         End If
-        Me.Hide()
+
     End Sub
 
 End Class
